f72e1041febdf622b7768c6de60ccc5e7922a6cd
Rich Trouton <rtrouton@yahoo.com>
2019-10-29 21:31:40 -0400

    Update VMwareGuestToolsURLProvider.py

--- VMware-Tools/VMwareGuestToolsURLProvider.py
+++ VMware-Tools/VMwareGuestToolsURLProvider.py
@@ -16,7 +16,7 @@ __all__ = ["VMwareGuestToolsURLProvider"]
 
 FUSION_URL_BASE = 'http://softwareupdate.vmware.com/cds/vmw-desktop/'
 DARWIN_TOOLS_URL_APPEND = 'packages/com.vmware.fusion.tools.darwin.zip.tar'
-DEFAULT_VERSION_SERIES = '11.1.0'
+DEFAULT_VERSION_SERIES = '11.5.0'
 
 class VMwareGuestToolsURLProvider(Processor):
     '''Provides URL to the latest Darwin ISO of the VMware Fusion tools.'''
